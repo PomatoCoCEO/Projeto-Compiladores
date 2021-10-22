ed83ae9fc06454a3416f8c25d8282f7bf4b5fc64
PomatoCoCEO <paulomiguelc@gmail.com>
2021-10-22 14:29:49 +0100

    Prototipo da AST a funcionar, nos com vectores de ponteiros

--- SYN_ANALYSIS/DEV/ast_tree.c
+++ SYN_ANALYSIS/DEV/ast_tree.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 typedef struct
 {
     void *array;
@@ -18,6 +20,8 @@ vector new_vector(size_t size_elem)
     vector ans;
     ans.size = ans.capacity = 0;
     ans.el_size = size_elem;
+    ans.array = NULL;
+    return ans;
 }
 
 void push_back(vector *vec, void *elem)
@@ -27,25 +31,136 @@ void push_back(vector *vec, void *elem)
         void *new_vec = malloc(vec->el_size);
         vec->array = new_vec;
         memcpy(vec->array, elem, vec->el_size);
-        // vec->size = 1;
         vec->capacity = 1;
+        vec->size++;
+        return;
     }
-    else if (vec->size >= vec->capacity)
+    if (vec->size >= vec->capacity)
     {
-        void *new_vec = malloc(2 * vec->capacity * vec->el_size);
-        memcpy(new_vec, vec->array, vec->el_size * vec->capacity);
+        void *new_vec = realloc(vec->array, 2 * vec->capacity * vec->el_size);
+        // memcpy(new_vec, vec->array, vec->el_size * vec->capacity);
         vec->capacity *= 2;
-        free(vec->array);
+        // free(vec->array);
+        vec->array = new_vec;
+    }
+
+    memcpy(vec->array + vec->size * vec->el_size, elem, vec->el_size);
+    vec->size++;
+    // free(elem); //! might be problematic if the thing is static
+}
+
+void push_back_ptr(vector *vec, void *elem)
+{
+    if (vec->size == 0)
+    {
+        void *new_vec = malloc(vec->el_size);
+        vec->array = new_vec;
+        memcpy(vec->array, elem, vec->el_size);
+        vec->capacity = 1;
+        vec->size++;
+        return;
+    }
+    if (vec->size >= vec->capacity)
+    {
+        void *new_vec = realloc(vec->array, 2 * vec->capacity * vec->el_size);
+        // memcpy(new_vec, vec->array, vec->el_size * vec->capacity);
+        vec->capacity *= 2;
+        // free(vec->array);
         vec->array = new_vec;
     }
     memcpy(vec->array + vec->size * vec->el_size, elem, vec->el_size);
     vec->size++;
-    free(elem);
+    // free(elem); //! might be problematic if the thing is static
 }
 
 void *get(vector *vec, int pos)
 {
     if (pos >= vec->size)
+    {
+        printf("Vec with no children...\n");
         return NULL;
+    }
     return (char *)vec->array + vec->el_size * pos;
+}
+
+void pop_back(vector *vec)
+{
+    if (vec->size == 0)
+        return;
+    vec->size--;
+    if (vec->size < vec->capacity / 4)
+    {
+        void *new_vec = realloc(vec->array, vec->el_size * vec->capacity / 2);
+        vec->capacity /= 2;
+    }
+}
+
+void test_vector()
+{
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector v = new_vector(sizeof(int));
+
+    for (int x = 0; x < sizeof(a) / sizeof(int); x++)
+        push_back(&v, &a[x]);
+
+    for (int i = 0; i < v.size; i++)
+    {
+        printf("v[%d] = %d\n", i, (*(int *)get(&v, i)));
+    }
+    printf("%ld\n", v.capacity);
+    for (int i = 0; i < 6; i++)
+        pop_back(&v);
+    for (int i = 0; i < v.size; i++)
+    {
+        printf("v[%d] = %d\n", i, (*(int *)get(&v, i)));
+    }
+    printf("%ld\n", v.capacity);
+}
+
+ast_node new_node(int node_type, char *name)
+{
+    ast_node ans;
+    ans.node_type = node_type;
+    int f = strlen(name) + 2;
+    ans.str = malloc(f * sizeof(char));
+    strcpy(ans.str, name);
+    ans.children = new_vector(sizeof(ast_node *));
+    return ans;
+}
+
+void add_child(ast_node *node, ast_node **child)
+{
+    push_back(&(node->children), /*(void *)(**/ child);
+    ast_node **ch = (ast_node **)node->children.array + node->children.size - 1; // get(&(node->children), node->children.size - 1);
+    printf("Child: %s\n", (*child)->str);
+    printf("Child: %s\n", (*ch)->str);
+}
+
+void remove_child(ast_node *node)
+{
+    pop_back(&(node->children));
+}
+
+void print_ast_tree(ast_node *node, int depth)
+{
+    /*if (node == NULL || node->children.size == 0)
+        return;*/
+    for (int i = 0; i < depth; i++)
+        printf("..");
+    printf("%s\n", node->str);
+    for (int i = 0; i < node->children.size; i++)
+    {
+        print_ast_tree(*((ast_node **)get(&(node->children), i)), depth + 1);
+    }
+}
+
+int main()
+{
+    ast_node a = new_node(0, "Father"), b = new_node(0, "Child1"), c = new_node(0, "Child2");
+    ast_node *f = &a, *g = &b, *h = &c;
+    add_child(&a, &g);
+    add_child(&a, &h);
+    print_ast_tree(&a, 0);
+    strcpy(b.str, "Heir?");
+    print_ast_tree(&a, 0);
 }
